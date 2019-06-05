1448b0e3024b5a7ea9f3bc59c4da9c83c589b258
Abdinasir <abdinasirnoor@outlook.com>
2019-06-04 20:25:25 -0400

    added code for test case to always fail. bug fixed for syntax error

--- src/tests/functional_tests.py
+++ src/tests/functional_tests.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
 import os
 import time
 import unittest
@@ -12,21 +10,22 @@ import unittest
 MAX_WAIT = 10
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(unittest.TestCase):
 
         def setUp(self):
             self.browser = webdriver.Firefox()
-            staging_server = os.environ.get('abdinasirnoor.com')
-            if staging_server:
-                self.live_server_url = 'https://' + staging_server
+
 
         def tearDown(self):
             self.browser.quit()
-
-        def test_static_files(self):
-
+        def browse_to_site(self):
+            self.browser.get('https://abdinasirnoor.com')
+            time.sleep(5)
+            get_http_response = self.browser.find_elements_by_tag_name('html')
+            self.AssertTrue(get_http_response)
+        self.fail('Finish the test!')
 #  Django test runner intiates the test therefore
 #  this code to begin app is unecessary. Keeping
 #  this code for historical sake
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
