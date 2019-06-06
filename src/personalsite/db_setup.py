0f97c4020e73ef6115e14d2dddfb2d931572e760
Abdinasir <abdinasirnoor@outlook.com>
2019-06-06 14:22:32 -0400

    added localhost and db script

---
+++ src/personalsite/db_setup.py
@@ -0,0 +1,28 @@
+import os
+import json
+from django.core.exceptions import ImproperlyConfigured
+import mysql.connector
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
+    secrets = json.load(secrets_file)
+
+def get_secret(setting, secrets=secrets):
+    """Get secret setting or fail with ImproperlyConfigured"""
+    try:
+        return secrets[setting]
+    except KeyError:
+        raise ImproperlyConfigured("Set the {} setting".format(setting))
+
+mydb = mysql.connector.connect(
+  user="root",
+  passwd=""
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute('CREATE DATABASE personalsite')
+
+mycursor.excute(f`CREATE USER abdi@localhost IDENTIFIED BY {get_secret('DB_PASSWORD')}; \
+                GRANT ALL ON personalsite TO abdi@localhost WITH GRANT OPTION;`)
