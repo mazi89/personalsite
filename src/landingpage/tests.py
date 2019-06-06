a2b8eb5667dee700d2523f4f6f7509657604c9f5
Abdinasir <abdinasirnoor@outlook.com>
2019-06-05 22:52:23 -0400

    updated to run django tests

--- src/landingpage/tests.py
+++ src/landingpage/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from myapp.models import *
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        contact_me.objects.create(
+        name_field = "abdi",
+        email_field = "mymail@mail.com",
+        message_text = "words of importance")
+        contact_me.objects.create(
+        name_field = "testDummy",
+        email_field = "mymail23@mail.com",
+        message_text = "words of importance2")
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        abdi = contact_me.objects.get(name="abdi")
+        testDummy = contact_me.objects.get(name="testDummy")
+        self.assertTrue(abdi)
+        self.assertTrue(testDummy)
