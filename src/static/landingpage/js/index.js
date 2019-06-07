e987c7909a1fd16efc5b63ea39166dfd46276ce2
Abdinasir <abdinasirnoor@outlook.com>
2019-06-06 21:11:01 -0400

    updated contact form

--- src/static/landingpage/js/index.js
+++ src/static/landingpage/js/index.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
 	form.addEventListener('submit', function(event){
 					event.preventDefault();
 					var request = new XMLHttpRequest();
-					request.open("POST", 'https://abdinasirnoor.com/contacted/', false);
+					request.open("POST", 'https://abdinasirnoor.com/contacted/', true);
 						request.onload = function() {
 							if ((this.status === 200)) {
 							 $("#contactModal").modal('hide'); // remove contactModal
 							 $("#successModal").modal('show'); //success message
+							 $(".btn-default").replaceWith('<p class="h2">Message has been sent <div class="far fa-thumbs-up"></p>');
 
 							}
 							else {
@@ -17,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 							$("#successModal").removeClass('alert-success');
 							$("#successModal").addClass('alert-danger');
 							$(".bodyMessage").replaceWith('<p class="h1 bodyMessage"><strong>Message Was not sent!</strong> bot detected. If you\re human please try again later!</p>');
+														 $(".btn-default").replaceWith('<p class="h2">Message has was not sent!<div class="far fa-thumbs-down"></p>');
 							};
 						};
 					request.send(new FormData(this));
