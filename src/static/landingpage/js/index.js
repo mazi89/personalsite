8383f61fb2ba3381fe64dbe6d045e9306a0badd1
Abdinasir <abdinasirnoor@outlook.com>
2019-06-06 20:19:55 -0400

    updated button call

--- src/static/landingpage/js/index.js
+++ src/static/landingpage/js/index.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 							 $("#contactModal").modal('hide'); // remove contactModal
 							 $('body').append(` <div class="alert alert-success alert-dismissible fade" role="alert" id="buttonAlert">
 											          <strong>Success!</strong> Message was sent successfully.
-											          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+											          <button type="button" class="show" data-dismiss="alert" aria-label="Close">
 											            <span aria-hidden="true">&times;</span>
 											          </button>
 											   				</div>`);
