3a3cf65bdd283f20bdf5ee7e9ebd5ab8d31eac25
Abdinasir <abdinasirnoor@outlook.com>
2019-06-06 21:16:59 -0400

    visuals

--- src/static/landingpage/js/index.js
+++ src/static/landingpage/js/index.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
 							if ((this.status === 200)) {
 							 $("#contactModal").modal('hide'); // remove contactModal
 							 $("#successModal").modal('show'); //success message
-							 $(".btn-default").replaceWith('<p class="h2">Message has been sent <div class="far fa-thumbs-up fa-3x"></p>');
+							 $(".btn-default").replaceWith('<p class="h2">Message has been sent <i class="far fa-thumbs-up fa-2x"></i></p>');
 
 							}
 							else {
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 							$("#successModal").removeClass('alert-success');
 							$("#successModal").addClass('alert-danger');
 							$(".bodyMessage").replaceWith('<p class="h1 bodyMessage"><strong>Message Was not sent!</strong> bot detected. If you\re human please try again later!</p>');
-							$(".btn-default").replaceWith('<p class="h2">Message has was not sent!<i class="far fa-thumbs-down fa-3x"></i></p>');
+							$(".btn-default").replaceWith('<p class="h2">Message has was not sent!<i class="far fa-thumbs-down fa-2x"></i></p>');
 							};
 						};
 					request.send(new FormData(this));
