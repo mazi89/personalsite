90ab4df5bd5cf559ecae8e5360aa8e8d99f98e34
Abdinasir <abdinasirnoor@outlook.com>
2019-06-06 20:58:05 -0400

    updated success and fail messages

--- src/static/landingpage/js/index.js
+++ src/static/landingpage/js/index.js
@@ -9,21 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
 						request.onload = function() {
 							if ((this.status === 200)) {
 							 $("#contactModal").modal('hide'); // remove contactModal
-							 $('body').append(` <div class="alert alert-success alert-dismissible fade" role="alert" id="buttonAlert">
-											          <strong>Success!</strong> Message was sent successfully.
-											          <button type="button" class="show" data-dismiss="alert" aria-label="Close">
-											            <span aria-hidden="true">&times;</span>
-											          </button>
-											   				</div>`);
-							 $('alert-success').alert();
+							 $("#successModal").modal('show'); //success message
+
 							}
 							else {
 							 $("#contactModal").modal('hide'); // remove contactModal
- 							 var node = document.createElement("div");
- 							 node.classList.add("alert");
- 							 node.classList.add("alert-danger");
- 							 node.textContent = "Message sent unsuccessfully! Bot detected. If you're human, please try again later.";
- 							 $(node).alert();
+							$("#successModal").removeClass('alert-success');
+							$("#successModal").addClass('alert-danger');
+							$(".bodyMessage").replaceWith('<p class="h1 bodyMessage"><strong>Message Was not sent!</strong> bot detected. If you\re human please try again later!</p>');
 							};
 						};
 					request.send(new FormData(this));
