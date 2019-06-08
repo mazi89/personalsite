4639bda12489cbe9998994c3fe0265dabd558e21
Abdinasir <abdinasirnoor@outlook.com>
2019-06-08 16:41:01 -0400

    email instance debug

--- src/landingpage/models.py
+++ src/landingpage/models.py
@@ -134,7 +134,7 @@ class Reply(models.Model):
         return self.email
 @receiver(post_save, sender=Reply)
 def send_reply_mail(sender, instance, **kwargs):
-    email_address = instance.email.objects.get(email_field)
+    email_address = instance.email.objects.values_list('email_field',flat=True).first()
     subject = 'RE: abdinasirnoor.com'
     body = instance.objects.get(message)
     origin_address = 'Abdinasir@abdinasirnoor.com'
