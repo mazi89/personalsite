7354bcff2a98be6ba8eb0897598da9ac7461c465
Abdinasir <abdinasirnoor@outlook.com>
2019-06-15 17:31:09 -0400

    more debugging

--- src/landingpage/received_mail.py
+++ src/landingpage/received_mail.py
@@ -1,22 +1,24 @@
 import sys, os, mailbox, django
 from django.conf import settings
-from personalsite import settings
-settings.configure(settings, DEBUG=True)
+from django.core.management.base import BaseCommand
+from landingpage.models import Inbox
 
-from personalsite.landingpage.models import Inbox
-mbox = mailbox.Maildir('~/Maildir/new')
+class Command(BaseCommand):
+    help = 'send email notification'
 
-for message in mbox:
-    if message.is_multipart():
-        content = ''.join([part.get_payload(decode=True) for part in message.get_payload()])
-    else:
-        content = message.get_payload(decode=True).as_string()
-    inbox_object = Inbox(
-    delivered_to=message['Delivered-To'],
-    email_from=message['From'],
-    subject_field=message['Subject'],
-    message_body=content,
-    date_received=message['Received'],
-    )
-    inbox_object.save()
-    print('New message received')
+    def handle(self, *args, **options):
+        mbox = mailbox.Maildir('~/Maildir/new')
+        for message in mbox:
+            if message.is_multipart():
+                content = ''.join([part.get_payload(decode=True) for part in message.get_payload()])
+            else:
+                content = message.get_payload(decode=True).as_string()
+            inbox_object = Inbox(
+            delivered_to=message['Delivered-To'],
+            email_from=message['From'],
+            subject_field=message['Subject'],
+            message_body=content,
+            date_received=message['Received'],
+            )
+            inbox_object.save()
+            print('New message received')
