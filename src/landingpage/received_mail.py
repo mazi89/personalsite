c8dfe254414e14b869a70a8db4b99f131c192d52
Abdinasir <abdinasirnoor@outlook.com>
2019-06-15 17:23:12 -0400

    getting specific in imports

--- src/landingpage/received_mail.py
+++ src/landingpage/received_mail.py
@@ -2,7 +2,7 @@ import sys, os, mailbox, django
 from django.conf import settings
 settings.configure(DEBUG=True)
 
-from landingpage.models import Inbox
+from personalsite.landingpage.models import Inbox
 mbox = mailbox.Maildir('~/Maildir/new')
 
 for message in mbox:
