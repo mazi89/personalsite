056537d042f6ae64492103845a0566047ac404e1
Abdinasir <abdinasirnoor@outlook.com>
2019-06-15 16:53:02 -0400

    updated save script

--- src/landingpage/received_mail.py
+++ src/landingpage/received_mail.py
@@ -8,11 +8,12 @@ for message in mbox:
         content = ''.join([part.get_payload(decode=True) for part in message.get_payload()])
     else:
         content = message.get_payload(decode=True).as_string()
-    Inbox.create(
+    inbox_object = Inbox(
     delivered_to=message['Delivered-To'],
     email_from=message['From'],
     subject_field=message['Subject'],
     message_body=content,
     date_received=message['Received'],
     )
+    inbox_object.save()
     print('New message received')
