f85fa5e009ca42f6eea419fd9195fce278f367fd
Hector <hectoremil21@hotmail.com>
2020-07-17 22:32:00 -0400

    Create Article Entity model

---
+++ src/newsreader/Spring/somali-up-to-date/src/main/java/com/ledesmahector/somaliuptodate/Entity/Article.java
@@ -0,0 +1,57 @@
+package com.ledesmahector.somaliuptodate.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String link;
+    private String description;
+
+    public Article() {}
+
+    public Article(Long id, String title, String link, String description) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
