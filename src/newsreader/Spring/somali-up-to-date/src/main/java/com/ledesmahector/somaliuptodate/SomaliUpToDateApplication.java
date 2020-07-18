17256f9bbadc7fe2ea4b4c3efe714f63b5497e08
Hector <hectoremil21@hotmail.com>
2020-07-17 22:31:48 -0400

    Add Spring folder with initial Spring Boot Application.

---
+++ src/newsreader/Spring/somali-up-to-date/src/main/java/com/ledesmahector/somaliuptodate/SomaliUpToDateApplication.java
@@ -0,0 +1,13 @@
+package com.ledesmahector.somaliuptodate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomaliUpToDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomaliUpToDateApplication.class, args);
+	}
+
+}
---
+++ src/newsreader/Spring/somali-up-to-date/src/test/java/com/ledesmahector/somaliuptodate/SomaliUpToDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledesmahector.somaliuptodate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomaliUpToDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
